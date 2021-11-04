66a3966d2f7d45a6169add30f13e930796522e03
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-11-04 11:49:36 +0100

    first commit

---
+++ my_clock/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { DigitalClock } from './components';
+
+function App() {
+  return (
+    <div className="App">
+        <DigitalClock></DigitalClock>
+    </div>
+  );
+}
+
+export default App;
---
+++ my_clock/src/components/DigitalClock.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DigitalClock() {
+    return (
+        <div>
+        
+        </div>
+    )
+}
+
+export default DigitalClock
