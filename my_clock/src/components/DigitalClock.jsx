13363a3a43866c966d56dcfe78e56536e0866e3f
Ana Paula Morales Dulzaides <anapaulamoralesdulzaidesrgmail.com>
2021-11-04 12:04:35 +0100

    clockState

--- my_clock/src/components/DigitalClock.jsx
+++ my_clock/src/components/DigitalClock.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function DigitalClock() {
+    const [clockState,setClockState]=useState();
+    useEffect(()=>{
+        setInterval(()=>{
+            const date=new Date();
+            setClockState(date.toLocaleTimeString());
+        })
+    },[]);
     return (
         <div>
-        
+            <h2>{clockState}</h2>
         </div>
     )
 }
